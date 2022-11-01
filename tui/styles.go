f92e75697386c9d5ce281646056cba08d1747837
James Best <jim@justjibba.net>
2022-11-01 21:51:46 +0000

    Add margin to bottom of container

--- tui/styles.go
+++ tui/styles.go
@@ -4,5 +4,6 @@ import "github.com/charmbracelet/lipgloss"
 
 var DocStyle = lipgloss.NewStyle().
 	Padding(1, 2, 1, 2).
+	Margin(2).
 	Border(lipgloss.NormalBorder(), true).
 	BorderForeground(lipgloss.Color("#7D56F4"))
