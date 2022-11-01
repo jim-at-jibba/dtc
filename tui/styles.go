83ca28d748bbf5d914378f7e9059ccd73cd18071
James Best <jim@justjibba.net>
2022-11-01 21:46:30 +0000

    Moved first style to tui package

---
+++ tui/styles.go
@@ -0,0 +1,8 @@
+package tui
+
+import "github.com/charmbracelet/lipgloss"
+
+var DocStyle = lipgloss.NewStyle().
+	Padding(1, 2, 1, 2).
+	Border(lipgloss.NormalBorder(), true).
+	BorderForeground(lipgloss.Color("#7D56F4"))
