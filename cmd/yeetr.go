7ae79b8ea6edb2aa4722c401746cc77b6b7452a9
James Best <jim@justjibba.net>
2022-11-05 21:56:40 +0000

    Add first pass at yeet, needs tidying and styles

--- cmd/yeetr.go
+++ cmd/yeetr.go
@@ -4,23 +4,57 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
 
 // yeetrCmd represents the yeetr command
-var yeetrCmd = &cobra.Command{
-	Use:   "yeetr",
+var yeetCmd = &cobra.Command{
+	Use:   "yeet",
 	Short: "For sharing files.",
 	Long:  "Sharing is, by design, ephemeral, so, the link that yeetr provides will expire, after a given time or when the file is downloaded. The idea for this tool was lifted from https://www.npmjs.com/package/yeetr",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("yeetr called")
+		items := []list.Item{}
+		files, err := ioutil.ReadDir(".")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			if !f.IsDir() {
+				items = append(items, item{name: f.Name(), size: strconv.FormatInt(f.Size()/1000, 10) + "KB"})
+			}
+		}
+
+		m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+		m.list.Title = "What files do you want to yeet?"
+
+		p := tea.NewProgram(m, tea.WithAltScreen())
+
+		if err := p.Start(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
+
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(yeetrCmd)
+	rootCmd.AddCommand(yeetCmd)
 
 	// Here you will define your flags and configuration settings.
 
@@ -31,4 +65,125 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// yeetrCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type yeetResponse struct {
+	Link string
+}
+
+func Yeet(fileName string) (string, error) {
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	fw, err := writer.CreateFormFile("file", fileName)
+	if err != nil {
+		return "", err
+	}
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	_, err = io.Copy(fw, file)
+	if err != nil {
+		return "", err
+	}
+	writer.Close()
+	req, err := http.NewRequest("POST", "https://file.io/?expires=2m", bytes.NewReader(body.Bytes()))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	rsp, err := client.Do(req)
+	check(err)
+	defer rsp.Body.Close()
+	// b, err := io.ReadAll(rsp.Body)
+	// check(err)
+	decoder := json.NewDecoder(rsp.Body)
+	var y yeetResponse
+	err = decoder.Decode(&y)
+	check(err)
+	fmt.Println(y.Link)
+	if rsp.StatusCode != http.StatusOK {
+		log.Printf("Request failed with response code: %d", rsp.StatusCode)
+	}
+	return y.Link, nil
+}
+
+func (m model) yeetMsg() tea.Msg {
+	fmt.Println("Yeeting...", m.choice)
+	link, _ := Yeet(m.choice)
+	return uploadUrl{link: link}
+}
+
+type item struct {
+	name, size string
+}
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+func (i item) Title() string       { return i.name }
+func (i item) Description() string { return i.size }
+func (i item) FilterValue() string { return i.name }
+
+type model struct {
+	list    list.Model
+	choice  string
+	yeeting bool
+	link    string
+}
+
+type uploadUrl struct {
+	link string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c":
+			return m, tea.Quit
+
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = string(i.name)
+				m.yeeting = true
+			}
+			return m, m.yeetMsg
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+
+	case uploadUrl:
+		m.link = msg.link
+		m.yeeting = false
+		return m, nil // tea.Quit removes the string
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	if m.link == "" {
+		return docStyle.Render(m.list.View())
+	} else if m.yeeting {
+		return "yeeting..."
+	} else {
+		return m.link
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
